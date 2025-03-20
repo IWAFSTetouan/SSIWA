aa02ac02f14ff7ba10c701b1ad38ac8c1e4da663
enaanai <aenaanai@uae.ac.ma>
2025-03-20 22:28:52 +0000

    Initial commit

---
+++ src/main/java/univ/iwa/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package univ.iwa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+@Bean
+SecurityFilterChain securityFilterChain(HttpSecurity http, JwtRequestFilter jwtRequestFilter) throws Exception {
+http.csrf(csrf->csrf.disable())
+.authorizeHttpRequests(authz -> authz
+.requestMatchers("/authenticate").permitAll()
+.anyRequest().authenticated()
+)
+.sessionManagement(session -> session
+.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+)
+.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+return http.build();
+}
+@Bean
+AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+return authenticationConfiguration.getAuthenticationManager();
+}}
---
+++ src/main/java/univ/iwa/filter/JwtRequestFilter.java
@@ -0,0 +1,5 @@
+package univ.iwa.filter;
+
+public class JwtRequestFilter {
+
+}
